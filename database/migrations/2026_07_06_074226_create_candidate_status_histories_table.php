de8f937144d20d12dde546d16d5fc721cab7129c
RATHANA.SOKHA <RATHANA.SOKHA@pnc.local>
2026-07-09 09:39:07 +0700

    fix: campaign

--- database/migrations/2026_07_06_074226_create_candidate_status_histories_table.php
+++ database/migrations/2026_07_06_074226_create_candidate_status_histories_table.php
@@ -8,21 +8,6 @@ return new class extends Migration
 {
     public function up()
     {
-<<<<<<< HEAD
-       Schema::create('candidate_status_histories', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('candidate_id')
-          ->constrained()
-          ->cascadeOnDelete();
-    $table->foreignId('changed_by')
-          ->nullable()
-          ->constrained('users')
-          ->nullOnDelete();
-    $table->date('changed_at');
-     $table->enum('status', ['Pending', 'Approved', 'Rejected', 'Withdrawn', 'Held', 'Selected'])->default('Pending')->nullable();
-    $table->timestamps();
-    });
-=======
         Schema::create('candidate_status_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('candidate_id')->constrained()->onDelete('cascade');
@@ -31,7 +16,6 @@ return new class extends Migration
             $table->dateTime('changed_at');
             $table->timestamps();
         });
->>>>>>> 87cb885b5de82731a26b1818c20af4145087198b
     }
 
     public function down()
--- routes/campaign.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Campaign\CampaignController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('campaigns', CampaignController::class);
