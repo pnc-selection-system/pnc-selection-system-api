3701a87278ebf0a8887f1e61bb2c37a62a563db1
KING VOEUN <king.voeun@stundent.passerellesnumeriques.org>
2026-07-09 10:05:59 +0700

    feat: crud ngo parnter and contact pern (#63)

---
+++ app/Http/Requests/Api/NgoPartner/UpdateContactPersonRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Api\NgoPartner;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactPersonRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'full_name' => 'sometimes|required|string|max:100',
+            'email' => 'sometimes|required|email|max:100',
+            'phone' => 'sometimes|required|string|max:30',
+        ];
+    }
+}
