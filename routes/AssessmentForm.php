d9c6f934f97add9f9e8c55eaef9bc603d87a1bb0
KING VOEUN <king.voeun@stundent.passerellesnumeriques.org>
2026-07-13 14:17:54 +0700

    fix: route api file

--- routes/AssessmentForm.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\AssessmentForm\AssessmentFormController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:api')->group(function () {
-    Route::apiResource('assessment-forms', AssessmentFormController::class);
-});
--- routes/NgoPartner.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\NgoPartner\NgoPartnerController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:api')->group(function () {
-    Route::apiResource('ngo-partners', NgoPartnerController::class);
-});
